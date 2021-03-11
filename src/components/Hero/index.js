c522a86c3848f6e8e3a567b5246e038d9ccf8fba
TadjievCodes <muhammad2008.08@mail.ru>
2021-03-11 15:16:59 +0500

    fixing background

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,8 +1,9 @@
 import * as React from 'react'
-
 import styles from './styles.module.css'
-
 import classnames from 'classnames'
+
+import background from "../../img/hero.jpg";
+
 
 
 // Always add .hero from styles.module.css
@@ -15,7 +16,7 @@ const classes = classnames('hero', 'hero-lg', 'mb-3', styles.hero)
 function Hero() {
   return (
     
-    <div className="hero hero-lg mb-3">
+    <div className="hero hero-lg mb-3" style={{ backgroundImage: `url(${background})` }}>
       <div className="hero-body text-center text-light">
         <h1>Premium Property Finder</h1>
         <p className="mb-0">Bringing premium property right to your fingertips</p>
